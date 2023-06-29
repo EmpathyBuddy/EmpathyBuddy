ee9802fe14b4fc8f1c371a02516dccdf746a86c6
Emilio Moreno <emiliomoremil@gmail.com>
2023-06-29 15:38:17 +0200

    requests routes complete and working

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -25,9 +25,94 @@ router.post("/posts", (req, res, next)=>{
         })    
 })
 
-// GET /api/projects -  Retrieves all of the projects
+// GET /api/posts -  Retrieves all of the posts
+router.get('/posts', (req, res, next) => {
+    Post.find()
+      
+        .then(response => {
+            res.json(response)
+        })
+        .catch(err => {
+            console.log("error getting list of posts", err);
+            res.status(500).json({
+                message: "error getting list of posts",
+                error: err
+            });
+        })
+});
 
 
+//  GET /api/posts/:postId  -  Get details of a specific post by id
+router.get('/posts/:postId', (req, res, next) => {
+    
+    const { postId } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(postId)) {
+        res.status(400).json({ message: 'Specified id is not valid' });
+        return;
+    }
+
+
+    Post.findById(postId)
+        
+        .then(post => res.json(post))
+        .catch(err => {
+            console.log("error getting details of a post", err);
+            res.status(500).json({
+                message: "error getting details of a post",
+                error: err
+            });
+        })
+});
+
+
+
+// PUT /api/posts/:postId  -  Updates a specific post by id
+router.put('/posts/:postId', (req, res, next) => {
+    const { postId } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(postId)) {
+        res.status(400).json({ message: 'Specified id is not valid' });
+        return;
+    }
+
+    const newDetails = {
+        title: req.body.title,
+        description:req.body.description
+    }
+
+    Post.findByIdAndUpdate(postId, newDetails, { new: true })
+        .then((updatedPost) => res.json(updatedPost))
+        .catch(err => {
+            console.log("error updating post", err);
+            res.status(500).json({
+                message: "error updating post",
+                error: err
+            });
+        })
+});
+
+
+// DELETE /api/posts/:postId  -  Delete a specific post by id
+router.delete('/posts/:postId', (req, res, next) => {
+    const { postId } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(postId)) {
+        res.status(400).json({ message: 'Specified id is not valid' });
+        return;
+    }
+
+    Post.findByIdAndRemove(postId)
+        
+        .then(() => res.json({ message: `Request with id ${postId} was removed successfully.` }))
+        .catch(err => {
+            console.log("error deleting request", err);
+            res.status(500).json({
+                message: "error deleting request",
+                error: err
+            });
+        })
+});
 
 
 
